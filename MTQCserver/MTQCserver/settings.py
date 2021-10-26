667c96bd267e7b7f0e0a1bf8f92fdaeb3f843502
Javier Pellejero <javier_pellejer@hotmail.com>
2021-10-26 12:33:50 +0200

    Logger configured for console

--- MTQCserver/MTQCserver/settings.py
+++ MTQCserver/MTQCserver/settings.py
@@ -129,3 +129,25 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} -- {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
